9b85b9f712e3ddbbb245897d00a874226bb80112
Sarah Rose <sarahrose63089@gmail.com>
2020-06-04 21:18:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,7 +5,13 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-
+  {
+    railroads: {
+      pieces: 4
+      rent_in_dollars: {}
+      names: {}
+    }
+  }
 end
 
 def monopoly_with_third_tier
